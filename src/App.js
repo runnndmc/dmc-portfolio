4694c91d386ded2d737963974de99a78cc6f326f
Daynamarie <Dayna.christison@gmail.com>
2020-09-24 14:33:43 -0400

    open and basic home text

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h5>about</h5>
+        <h5>work</h5>
+        <button>Resume</button>
+      </header>
+      <main>
+        <h2>This is the main section</h2>
+      </main>
+      <aside>
+        <h3>Hello World</h3>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
