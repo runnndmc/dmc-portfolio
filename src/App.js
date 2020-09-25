c9c99ce1063b5c157483bb26cf29d50158708119
Daynamarie <Dayna.christison@gmail.com>
2020-09-24 22:40:22 -0400

    i think this is okay

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
+        <div className='tag'></div>
           <ul className='direct-links'>
             <Link to='/about'>about</Link>
             <Link to='/work'>work</Link>
@@ -14,9 +15,13 @@ function App() {
           </ul> 
       </header>
       <main>
-          <h3 className='hello'>Hello World!</h3>
+        <h3 className='hello'>Hello World!</h3>
       </main>
-      <h3 className='myname'>I'm Dayna Christison</h3>
+
+      <div className='kill-me'>
+        <h3 className='myname'>I'm Dayna Christison</h3>
+      </div>
+     
       <div className='title'>
         <h2 className='fullstack'>Full Stack Developer<span className='dot'>.</span></h2>
       </div>
