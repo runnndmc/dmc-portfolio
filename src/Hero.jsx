9ceb06a957defe35c650235d7d34b45e6f1a4c65
Daynamarie <Dayna.christison@gmail.com>
2020-09-25 21:29:17 -0400

    file structure

---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+
+    return(
+        <>
+            <h2>Hello World!</h2>
+            <h2>Hi, I'm Dayna Christison</h2>
+            <h3>Full Stack Developer</h3>
+        </>
+    )
+}
+
+export default Hero
---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+
+    return(
+        <>
+            <h5>About</h5>
+            <h5>Work</h5>
+            <h5>Contact</h5>
+            <button>Resume</button>
+        </>
+    )
+}
+
+export default Nav
