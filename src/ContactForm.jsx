6c37489a04b3215cd76a9caef6dbf47a7d339f0a
Daynamarie <Dayna.christison@gmail.com>
2020-09-28 19:25:00 -0400

    all links scrolling

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactForm = () => {
     return(
         <div className='contact-form'>
-            <h3>Let's Keep In Touch!</h3>
+            <h3><span id='contact'> &nbsp; </span>Let's Keep In Touch!</h3>
             <h5>Email:</h5>
             <a href='mailto: dayna.christison@gmail.com'>Send Me an Email</a>
         </div>    
