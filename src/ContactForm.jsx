ad50e8989e0bbf369410f5ad09fd3971ceab28e6
Daynamarie <Dayna.christison@gmail.com>
2020-09-28 21:16:34 -0400

    contact form set

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const ContactForm = () => {
     return(
         <div className='contact-form'>
-            <h3><span id='contact'> &nbsp; </span>Let's Keep In Touch!</h3>
-            <h5>Email:</h5>
-            <a href='mailto: dayna.christison@gmail.com'>Send Me an Email</a>
+            <h3 className='contact-title'><span id='contact'> &nbsp; </span>Let's Keep In Touch!</h3>
+            <div className='contact-links'>
+                {/* <h5 className='label'>Just </h5> */}
+                <a href='mailto: dayna.christison@gmail.com' className='email-link'> Send Me an Email</a>
+            </div>
         </div>    
     )
 }
