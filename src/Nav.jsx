b58939d18f991e0e87ecdee4291584d8bb0efb09
Daynamarie <Dayna.christison@gmail.com>
2020-09-30 12:47:46 -0400

    media q on about section set - footer larger - hello larger - canva switch

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
                 <a href='#about'><h5 className='about-tag'>ABOUT</h5></a>
                 <a href='#work'><h5 className='work-tag'>WORK</h5></a>
                 <a href='#contact'><h5 className='contact-tag'>CONTACT</h5></a>
-                <a href='https://www.canva.com/design/DAEDCV1YT_8/view?utm_content=DAEDCV1YT_8&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink'><button>Resume</button></a>
+                <a href='https://drive.google.com/file/d/1o-oZ5nSibfpzpclIAOlCRg9o4V2YiA_q/view?usp=sharing' target="_blank"><button>Resume</button></a>
             </ul>
         </div>
     )
