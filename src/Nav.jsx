4e2850d0badbd5e3584b5d951e48abc1bc86e920
Daynamarie <Dayna.christison@gmail.com>
2020-09-30 17:22:24 -0400

    new resume link

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
                 <a href='#about'><h5 className='about-tag'>ABOUT</h5></a>
                 <a href='#work'><h5 className='work-tag'>WORK</h5></a>
                 <a href='#contact'><h5 className='contact-tag'>CONTACT</h5></a>
-                <a href='https://drive.google.com/file/d/1o-oZ5nSibfpzpclIAOlCRg9o4V2YiA_q/view?usp=sharing' target="_blank"><button>Resume</button></a>
+                <a href='https://drive.google.com/file/d/1k_wNLq3TvGvQ8nmJBkKg7FrS1E7YoiGu/view?usp=sharing' target="_blank"><button>Resume</button></a>
             </ul>
         </div>
     )
