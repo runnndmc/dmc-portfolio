41010acbd1aeae4b58629460f1f66df0a15702af
Daynamarie <Dayna.christison@gmail.com>
2020-11-16 15:03:11 -0500

    changed contact form to have mail be icon

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-
+import React from "react";
 
 const ContactForm = () => {
+  return (
+    <div className="contact-form">
+      <h3 className="contact-title">
+        <span id="contact"> &nbsp; </span>Let's Keep In Touch!
+      </h3>
 
-    return(
-        <div className='contact-form'>
-            <h3 className='contact-title'><span id='contact'> &nbsp; </span>Let's Keep In Touch!</h3>
-            <div className='contact-links'>
-               <a href='mailto: dayna.christison@gmail.com' className='email-link'> Send Me an Email</a>
-              
-            </div> 
-            <div className='contact-links'>
-                <a href='https://www.linkedin.com/in/daynachristison/' >
-                <img className='linked-in' src="./linkedin.png"/>
-                </a>
-                <a href='https://github.com/runnndmc'>
-                <img className='linked-in'src="./github.png"/>
-                </a>
-            </div>
-        </div>    
-    )
-}
+      <div className="contact-links">
+        <a href="mailto: dayna.christison@gmail.com" className="email-link">
+          {" "}
+          <img className="mail-link" src="./mail.png" alt="envelope to e-mail"/>
+        </a>
 
-export default ContactForm
+        <a href="https://www.linkedin.com/in/daynachristison/">
+          <img className="linked-in" src="./linkedin.png" alt="linkedin logo"/>
+        </a>
+        <a href="https://github.com/runnndmc">
+          <img className="linked-in" src="./github.png" alt="github logo"/>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
