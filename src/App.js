541de0bfe11d88dea98fb93d2133ab9756526f8c
Daynamarie <Dayna.christison@gmail.com>
2021-05-10 14:57:54 -0400

    projectsList passed as a prop from index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import AboutMe from "./AboutMe";
+import Work from "./Work";
+import ContactForm from "./ContactForm";
 
-import Header from './Header';
-import Hero from './Hero';
-import Nav from './Nav';
-import AboutMe from './AboutMe';
-import Work from './Work';
-import ContactForm from './ContactForm';
-
-function App() {
+const App = ({ projectsList }) => {
+  //try doing switch and root
+  //header contains the Nav - changes to layout?
+  //exact path="/about" component={About}
+  //exact path="/work" component={Work} - Projects?
+  //exact path="/Contact" component={Contact}
+console.log(projectsList)
   return (
-    <div className='body'>
-      <span id='home'> &nbsp; </span>
-      <Header/>
+    <div className="body">
+      <span id="home"> &nbsp; </span>
+
+      <Header />
+      {/* switch to Logo or something else */}
+
       <Hero />
       <aside></aside>
       <Nav />
+
       <AboutMe />
+      {/* switch to About  */}
       <Work />
       <ContactForm />
     </div>
   );
-}
+};
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,14 +3,62 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+const projectsList = [
+  {
+    id: 1,
+    title: "Still Positive",
+    photoLink: {
+      href:"https://stillpositive.netlify.app/",
+      src:"https://i.imgur.com/XqKz45j.png",
+    },
+    devIcons: {
+      src1:"https://img.icons8.com/color/48/000000/react-native.png",
+      src2:"https://img.icons8.com/color/48/000000/css3.png",
+      src3:"./airtable.png"
+    },
+    site:"https://stillpositive.netlify.app/",
+    github:"https://github.com/runnndmc/still-positive"
+  },
+  {
+    id: 2,
+    title: "New Alternatives",
+    photoLink: {
+      href:"http://new-alternatives-site.surge.sh/",
+      src: "https://i.imgur.com/1aUVjGC.png"
+    },
+    devIcons: {
+      src1:"https://img.icons8.com/color/48/000000/mongodb.png",
+      src2:"https://img.icons8.com/color/48/000000/nodejs.png",
+      src3:"https://img.icons8.com/color/48/000000/react-native.png"
+    },
+    site:"http://new-alternatives-site.surge.sh/",
+    github:"https://github.com/chrisratsimba123/new-alternatives-app/"
+  },
+  {
+    id: 3,
+    title: "Movie Worm",
+    photoLink: {
+      href:"https://moiveworm.netlify.app",
+      src:"https://i.imgur.com/U5lC9z8.png"
+    },
+    devIcons: {
+      src1:"./ruby.png",
+      src2:"./rails.png",
+      src3:"https://img.icons8.com/color/48/000000/react-native.png"
+    },
+    site:"https://moiveworm.netlify.app",
+    github:"https://github.com/runnndmc/MovieWorm"
+  }
+];
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Router>
+    <App projectsList={projectsList}/>
+  </Router>,
   document.getElementById('root')
 );
 
