b31c6560393a1cb6893676c579b875a93d3eaeae
Daynamarie <Dayna.christison@gmail.com>
2021-05-12 16:58:12 -0400

    set interval and clearInterval working

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,9 +4,29 @@ import Layout from "../shared/layout/Layout";
 import "./hero.css";
 
 const Hero = () => {
+    const colorList = [
+        "#e86521",
+        "#ff9650",
+        "#af3500",
+    ]
+    let index = 0;
+
+
+function changeBackground(){
+   const changeColor = setInterval(function(){
+        console.log(index)
+        index++
+        if(index === 3){
+            clearInterval(changeColor)
+        }
+    }, 1000)
+
+
+
+} 
   return (
     <Layout>
-      <div className="hero">
+      <div className="hero" onMouseEnter={changeBackground}>
         <p className="hello">Hello World! I'm</p>
         <h2 className="intro">Dayna Christison</h2>
         <p className="job-title">Full Stack Developer [ She / Her / Hers ]</p>
