b05d7963a2c52bda417c09ac02412302167c81da
Daynamarie <Dayna.christison@gmail.com>
2021-05-13 12:08:56 -0400

    media querie on about feels good placement wise

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,22 +8,23 @@ const AboutMe = () => {
     <Layout>
       <span id="about"> &nbsp; </span>
       <div className="about-section">
-
-      <div className="about-info">
-        <p className="statement">
-          I'm passionate about delivering end-to-end experiences and care about
-          the software architecture across the frontend, backend, and the APIs
-          that glue them together. By building full stack applications, I aim to
-          implement clean, effective technology that can directly affect the
-          consumers understanding of a product.
-        </p>
-        <div className="line-wrapper">
-          <div className="line"></div>
+        <div className="about-info">
+          <p className="statement">
+            I'm passionate about delivering end-to-end experiences and care
+            about the software architecture across the frontend, backend, and
+            the APIs that glue them together. By building full stack
+            applications, I aim to implement clean, effective technology that
+            can directly affect the consumers understanding of a product.
+          </p>
+          <div className="line-wrapper">
+            <div className="line"></div>
+          </div>
+          <img className="about-photo" src="../filler.webp" alt="filler" />
         </div>
-        <img className="about-photo"src="../filler.webp" alt="filler" />
-      </div>
 
-      <div className="question-block">"Tell me about yourself!"</div>
+        <div className="question-block">
+          <h2 className="question-text">"Tell me about yourself!"</h2>
+        </div>
       </div>
     </Layout>
   );
