ce2f4c63f9e21f725940a2893574758f4af78686
Daynamarie <Dayna.christison@gmail.com>
2021-05-13 10:13:39 -0400

    hero css changes on hover

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Layout from "../shared/layout/Layout";
 import "./hero.css";
 
 const Hero = () => {
-    const colorList = [
+/*     const colorList = [
         "#e86521",
         "#ff9650",
         "#af3500",
@@ -14,21 +14,22 @@ const Hero = () => {
 
 function changeBackground(){
    const changeColor = setInterval(function(){
-        document.querySelector('.hero').style.backgroundColor = colorList[index]
+
+      document.querySelector('.hero').style.backgroundColor = colorList[index]
         console.log(colorList[index])
+
         index++
         if(index >= 3){
             clearInterval(changeColor)
             index = 0
-        }
-    }, 1000)
+        } 
+    }, 1000) */
 
 
 
-} 
   return (
     <Layout>
-      <div className="hero" onMouseEnter={changeBackground}>
+      <div className="hero"/*  onMouseEnter={changeBackground} */>
         <p className="hello">Hello World! I'm</p>
         <h2 className="intro">Dayna Christison</h2>
         <p className="job-title">Full Stack Developer [ She / Her / Hers ]</p>
