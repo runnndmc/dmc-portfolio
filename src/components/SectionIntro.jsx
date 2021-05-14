33d300f9479e7d764218d3f701bf3ca1ba820f40
Daynamarie <Dayna.christison@gmail.com>
2021-05-14 10:47:11 -0400

    vertical and horizontal lines in place for section intro

--- src/components/SectionIntro.jsx
+++ src/components/SectionIntro.jsx
@@ -5,6 +5,7 @@ const SectionIntro = () => {
     return(
         <div className="section-intro">
             <div className="vertical-line"></div>
+            <div className="horizontal-line"></div>
         </div>
     )
 }
