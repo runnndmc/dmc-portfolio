a2778e033b96816a87f7e88e9a28901c8d2f0514
Daynamarie <Dayna.christison@gmail.com>
2021-05-14 15:00:23 -0400

    button adjust and project card looking good

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,34 +2,49 @@ import React from "react";
 import "./project.css";
 
 const Project = ({ project }) => {
-
- function openLink(link){
-    window.open(link)
- }
+  function openLink(link) {
+    window.open(link);
+  }
 
   return (
-  <div className="project-wrapper">
+    <div className="project-wrapper">
       <h3 className="title">{project.title}</h3>
       <div className="built-with">
-        <img className="dev-icon" src={project.devIcons.src1} alt="Tech stack Icon"/>
-        <img className="dev-icon" src={project.devIcons.src2} alt="Tech stack Icon"/>
-        <img className="dev-icon" src={project.devIcons.src3} alt="Tech stack Icon"/>
-    </div>
         <img
-            className="website-still"
-            src= {project.photoLink.src}
-            onClick= {() => openLink(`${project.photoLink.href}`)}
-            alt="Project preview"
+          className="dev-icon"
+          src={project.devIcons.src1}
+          alt="Tech stack Icon"
         />
-    <div className="text">
+        <img
+          className="dev-icon"
+          src={project.devIcons.src2}
+          alt="Tech stack Icon"
+        />
+        <img
+          className="dev-icon"
+          src={project.devIcons.src3}
+          alt="Tech stack Icon"
+        />
+      </div>
+      <img
+        className="website-still"
+        src={project.photoLink.src}
+        onClick={() => openLink(`${project.photoLink.href}`)}
+        alt="Project preview"
+      />
+      <div className="text">
         <p>{project.description}</p>
+      </div>
+      <button className="lookup" onClick={() => openLink(`${project.github}`)}>
+        Code
+        <img
+          className="github-icon"
+          src="../contact-icons/github.png"
+          alt="github-icon"
+        />
+      </button>
     </div>
-    <div className="button-container">
-        <button className="lookup" onClick={() => openLink(`${project.site}`)}>Site</button>
-        <button className="lookup" onClick={() => openLink(`${project.github}`)}>Code</button>
-    </div>
-  </div>
-  )
+  );
 };
 
 export default Project;
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const projectsList = [
     title: "Still Positive",
     photoLink: {
       href:"https://stillpositive.netlify.app/",
-      src:"https://i.imgur.com/XqKz45j.png",
+      src:"../project-photos/StillPositive.png",
     },
     devIcons: {
       src1:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
@@ -20,7 +20,6 @@ const projectsList = [
       src3:"./dev-icons/react-small.png"
     },
     description: "A PWA with emphasis on accessibility. The site was created to host the podcast Still Positive for people who are chronically ill and/or disabled. Built with React Hooks with complete responsive modern design, Still Positive utilizes Airtable's API to fetch and store the data.",
-    site:"https://stillpositive.netlify.app/",
     github:"https://github.com/runnndmc/still-positive"
   },
   {
@@ -36,7 +35,6 @@ const projectsList = [
       src3:"./dev-icons/react-small.png"
     },
     description: "In collaboration with a UX design team and three other software engineers, built a full-stack MERN CRUD site which took an already existing non-profit website and fully redesigned it. New Alternatives is a user friendly and interactive site with user testimonials and video carousels.",
-    site:"http://new-alternatives-site.surge.sh/",
     github:"https://github.com/chrisratsimba123/new-alternatives-app/"
   },
   {
@@ -52,7 +50,6 @@ const projectsList = [
       src3:"./dev-icons/react-small.png"
     },
     description: "Built a on Rails server, with three database tables which included user authentication and a join table. Created an interactive React front-end with full CRUD on all movies and the ability to create a review and rating for each specific movie.",
-    site:"https://moiveworm.netlify.app",
     github:"https://github.com/runnndmc/MovieWorm"
   },
   {
@@ -68,7 +65,6 @@ const projectsList = [
       src3:"./dev-icons/react-small.png"
     },
     description: "A single page web application for a featured special running on a particular brand of hearing aid. The offer is only given to those who sign up with the given companies' newsletter list. Built with a React architecture with JavaScript code utilizing CSS and Flexbox for styling.",
-    site:"https://earq.netlify.app/",
     github:"https://github.com/runnndmc/ear-q-landing"
   }
 ];
