3a6e5a9373fbaf24428eb604e67b0c86d993a2a3
Daynamarie <Dayna.christison@gmail.com>
2021-05-14 11:51:13 -0400

    adjusting the project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,17 +10,17 @@ const Project = ({ project }) => {
   return (
   <div className="project-wrapper">
       <h3 className="title">{project.title}</h3>
+      <div className="built-with">
+        <img className="dev-icon" src={project.devIcons.src1} alt="Tech stack Icon"/>
+        <img className="dev-icon" src={project.devIcons.src2} alt="Tech stack Icon"/>
+        <img className="dev-icon" src={project.devIcons.src3} alt="Tech stack Icon"/>
+    </div>
         <img
             className="website-still"
             src= {project.photoLink.src}
             onClick= {() => openLink(`${project.photoLink.href}`)}
             alt="Project preview"
         />
-    <div className="built-with">
-        <img className="dev-icon" src={project.devIcons.src1} alt="Tech stack Icon"/>
-        <img className="dev-icon" src={project.devIcons.src2} alt="Tech stack Icon"/>
-        <img className="dev-icon" src={project.devIcons.src3} alt="Tech stack Icon"/>
-    </div>
     <div className="text">
         <p>{project.description}</p>
     </div>
