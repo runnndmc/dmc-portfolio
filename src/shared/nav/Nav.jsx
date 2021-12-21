dac811e0695ec6cc1645892e86f2a2407c2b70fd
Daynamarie <Dayna.christison@gmail.com>
2021-12-21 17:33:10 -0500

    nav aligment and className update

--- src/shared/nav/Nav.jsx
+++ src/shared/nav/Nav.jsx
@@ -1,51 +1,27 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FaGithubAlt } from "react-icons/fa";
-
-
+import { FaGithubAlt, FaLinkedin } from "react-icons/fa";
 import "./nav.css";
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav__wrapper">
-        <a href="#work">
-          <button className="nav__work-btn">Work</button>
-        </a>
-        <div className="connect-icons">
-          <FaGithubAlt/>
-          {/* <a
-            href="https://github.com/runnndmc"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="tag"
-          >
-            <img
-              className="github-icon"
-              src="../contact-icons/github.png"
-              alt="github-icon"
-            />
-          </a> */}
-          <a
-            href="https://www.linkedin.com/in/daynachristison/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="tag"
-          >
-            <img
-              className="linkedin-icon"
-              src="../contact-icons/linkedin.png"
-              alt="LinkedIn-icon"
-            />
+
+        <a href="#work" className="nav__button-a"><button className="nav__work-btn">Work</button></a>
+
+        <div className="nav__icons-wrapper">
+          <a href="https://github.com/runnndmc" className="nav__social-icons" target="_blank" rel="noopener noreferrer">
+            <FaGithubAlt style={{fontSize: '50px'}}/>
+          </a>
+          <a href="https://www.linkedin.com/in/daynachristison/" className="nav__social-icons" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin style={{fontSize: '45px'}}/>
           </a>
         </div>
-        <a
-          href="https://drive.google.com/file/d/1k_wNLq3TvGvQ8nmJBkKg7FrS1E7YoiGu/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="resume-link">Resume</button>
+
+        <a href="https://drive.google.com/file/d/1k_wNLq3TvGvQ8nmJBkKg7FrS1E7YoiGu/view?usp=sharing" className="nav__button-a" target="_blank"rel="noopener noreferrer">
+          <button className="nav__resume-btn">Resume</button>
         </a>
+
       </div>
     </nav>
   );
@@ -56,4 +32,4 @@ export default Nav;
 /* ===== NOTES ===== */
 // clean up css
 // button update!
-// icons from font-awesome && removed from icons
+// check responsiveness
