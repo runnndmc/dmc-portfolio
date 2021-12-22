7bde6323f1d3f7eae7aad7f0de5c59a851cb2128
Daynamarie <Dayna.christison@gmail.com>
2021-12-22 17:22:33 -0500

    about adjusting photo and line - removing div

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -8,9 +8,8 @@ const AboutMe = () => {
     <Layout>
       <span id="about"> &nbsp; </span>
       <div className="about-section">
-
-        <div className="about-info">
-          <p className="statement">
+        <div className="about__info-wrapper">
+          <p className="about__statement">
             I'm passionate about delivering end-to-end experiences and care
             about the software architecture across the frontend, backend, and
             the APIs that glue them together. By building full stack
@@ -18,17 +17,18 @@ const AboutMe = () => {
             can directly affect the consumers understanding of a product.
           </p>
 
-          <div className="line-wrapper">
-            <div className="line"></div>
-          </div>
+          <div className="line"></div>
 
-          <img className="about-photo" src="../profile.png" alt="a white woman with showrt brown hair wearing a dark gret knit sweater and a silver necklace chain" />
+          <img
+            className="about-photo"
+            src="../profile.png"
+            alt="a white woman with showrt brown hair wearing a dark gret knit sweater and a silver necklace chain"
+          />
         </div>
 
         <div className="question-block">
           <h2 className="question-text">"Tell me about yourself!"</h2>
         </div>
-
       </div>
     </Layout>
   );
@@ -40,11 +40,11 @@ export default AboutMe;
 // check up on statement -  add EarQ
 // line wrapper needed ?
 // about-info -> about-me__wrapper
-// about-me__photo name 
-// img accessible? 
+// about-me__photo name
+// img accessible?
 // check height on question block - try to fit on fold
 // padding-right needed?
 // flex wrap on about-section instead of about-info?
-// photo adjust.. instead of height and width and border adjust, 
+// photo adjust.. instead of height and width and border adjust,
 // full section - raised background with just shadow instead of border and annimation to arrive on scroll
 //
