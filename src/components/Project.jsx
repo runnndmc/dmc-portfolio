92ee3f956e3e6182c8ab2d2344a70270e8f0bf4d
Daynamarie <Dayna.christison@gmail.com>
2022-01-05 11:46:06 -0500

    video play on hover and pause on exit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,36 +7,59 @@ const Project = ({ project }) => {
     window.open(link);
   }
 
+  
+
   return (
     <div className="project__card">
         <h3 className="project__title" onClick={() => openLink(`${project.photoLink.src}`)}>{project.title}</h3>
-        <iframe
+        
+        
+        <div className="project__video-container">
+            <div className="project__video-box">
+              <video 
+                className='project__clip' 
+                onMouseEnter={(e) => e.target.play()}
+                onMouseLeave={(e) => e.target.pause()}
+                src="../videos/dmc_portfolio-earq.mp4" 
+                type="video/mp4">
+              </video>
+            </div>
+        </div>
+
+
+        {/* <iframe
           className="website-still"
           src={project.photoLink.src}
           title="Project preview"
-        ></iframe>
+        ></iframe> */}
+
+
       <div className="text">
         <p className="text">{project.description}</p>
       </div>
+
+
       <div className="project__button-wrapper">
-      <button className="lookup" onClick={() => openLink(`${project.photoLink.src2}`)}>
-      {/* <img
-          className="github-icon-btn"
-          src="../contact-icons/github.png"
-          alt="github-icon"
-          width="40px"
-        /> */}
-        Site
-      </button>     
-      <button className="lookup" onClick={() => openLink(`${project.github}`)}>
-      {/* <img
-          className="github-icon-btn"
-          src="../contact-icons/github.png"
-          alt="github-icon"
-          width="40px"
-        /> */}
-        Code
-      </button>
+        <button className="lookup" onClick={() => openLink(`${project.photoLink.src2}`)}>
+        {/* <img
+            className="github-icon-btn"
+            src="../contact-icons/github.png"
+            alt="github-icon"
+            width="40px"
+          /> */}
+          Site
+        </button>     
+
+        <button className="lookup" onClick={() => openLink(`${project.github}`)}>
+        {/* <img
+            className="github-icon-btn"
+            src="../contact-icons/github.png"
+            alt="github-icon"
+            width="40px"
+          /> */}
+          Code
+        </button>
+
       </div>
     </div>
   );
