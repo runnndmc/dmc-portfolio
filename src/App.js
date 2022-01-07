c030545b591b9556b436f652ebaa8067d61be102
Daynamarie <Dayna.christison@gmail.com>
2022-01-07 17:04:53 -0500

    added tech to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./screens/Hero";
 import About from "./screens/About";
 import Work from "./screens/Work";
 import Contact from "./screens/Contact";
+import Tech from "./screens/Tech";
 
 const App = ({ projectsList }) => {
   return (
@@ -13,6 +14,7 @@ const App = ({ projectsList }) => {
       <Hero />
       <About />
       <Work projectsList={projectsList} />
+      <Tech/>
       <Contact />
     </div>
   );
