d966af6758121dd460c2a9d847746d406ed0461d
DaynaChristison <daynamarie1743@gmail.com>
2022-10-10 14:17:04 -0400

    fixed the about section and made it responsive - removed the line

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -8,6 +8,13 @@ const AboutMe = () => {
       <span id="about"> &nbsp; </span>
       <div className="about-section">
         <div className="about__info-wrapper">
+
+        <img
+            className="about-photo"
+            src="../profile.png"
+            alt="Dayna, a white woman with short broown hair smiling against a purple mountian painting"
+          />
+
           <p className="about__statement">
             I'm passionate about delivering end-to-end experiences and care
             about the software architecture across the frontend, backend, and
@@ -16,13 +23,6 @@ const AboutMe = () => {
             can directly affect the consumers understanding of a product.
           </p>
 
-          <div className="line"></div>
-
-          <img
-            className="about-photo"
-            src="../profile.png"
-            alt="a white woman with showrt brown hair wearing a dark gret knit sweater and a silver necklace chain"
-          />
         </div>
 
         <div className="question-block">
@@ -34,16 +34,3 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
-
-/* ===== NOTES ===== */
-// check up on statement -  add EarQ
-// line wrapper needed ?
-// about-info -> about-me__wrapper
-// about-me__photo name
-// img accessible?
-// check height on question block - try to fit on fold
-// padding-right needed?
-// flex wrap on about-section instead of about-info?
-// photo adjust.. instead of height and width and border adjust,
-// full section - raised background with just shadow instead of border and annimation to arrive on scroll
-//
